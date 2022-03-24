a584e7f320b5bd342253e85471437b46aea5cbde
Monika Lenartowicz <nika.nel@interia.pl>
2022-03-24 15:20:02 +0100

    Added checking errors

--- script.js
+++ script.js
@@ -2,17 +2,45 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const password2 = document.querySelector("#password2");
 const email = document.querySelector("#email");
-
 const sendButton = document.querySelector(".send");
 const clearButton = document.querySelector(".clear");
 const closeButton = document.querySelector(".close");
 const popup = document.querySelector(".popup");
+const formInputs = [username, password, password2, email];
+
+const showError = (input, message) => {
+	const formBox = input.parentElement;
+	const errorMessage = formBox.querySelector(".error-text");
+	formBox.classList.add("form-box__error");
+	errorMessage.textContent = message;
+};
+
+const clearError = input => {
+	const formBox = input.parentElement;
+	formBox.classList.remove("form-box__error");
+};
+
+const checkForm = input => {
+	input.forEach(element => {
+		if (element.value === "") {
+			showError(element, element.placeholder);
+		} else {
+			clearError(element);
+		}
+	});
+};
 
 const clear = event => {
 	event.preventDefault();
-	[username, password, password2, email].forEach(element => {
+	formInputs.forEach(element => {
 		element.value = "";
 	});
 };
 
+const send = event => {
+	event.preventDefault();
+	checkForm(formInputs);
+};
+
 clearButton.addEventListener("click", clear);
+sendButton.addEventListener("click", send);
