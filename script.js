6bf893ba6c8fcf3a43c2fbd0752ac83a4c63df88
Monika Lenartowicz <nika.nel@interia.pl>
2022-03-24 11:59:32 +0100

    Added function clear on button clear

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+const email = document.querySelector("#email");
+
+const sendButton = document.querySelector(".send");
+const clearButton = document.querySelector(".clear");
+const closeButton = document.querySelector(".close");
+const popup = document.querySelector(".popup");
+
+const clear = event => {
+	event.preventDefault();
+	[username, password, password2, email].forEach(element => {
+		element.value = "";
+	});
+};
+
+clearButton.addEventListener("click", clear);
